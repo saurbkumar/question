fb77e85db3d61502e0e96dfb7b3d87ec283eaa57
Saurabh Kumar <saurabhkumar.spsu@gmail.com>
2018-07-27 12:26:49 -0400

    Create binary-tree-kth-smallest.py

---
+++ my_question/binary-tree-kth-smallest.py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+root = Node(100)
+root.left = Node(60)
+root.right = Node(130)
+root.right.right = Node(170)
+root.right.left = Node(120)
+root.left.left = Node(50)
+root.left.right = Node(65)
+path = []
+result  = None
+#
+def getLeft(root,k):
+    node = root
+    stack = []
+    count = 0
+    while stack!=[] or node!=None:
+        if node:
+            stack.append(node)
+            node = node.left
+        else:
+            
+            temp_node = stack.pop()
+            count = count + 1
+            if count==k:
+                return temp_node
+            else:
+                  if node:
+                    node = node.right
+            
+    return None
+data = getLeft(root,1)
+print(data.data)
