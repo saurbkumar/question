af5d8cde2de8ef8b1c7ee625098cb92a982aa477
Saurabh Kumar <saurabhkumar.spsu@gmail.com>
2018-07-28 12:40:12 -0400

    Update array-next-greater.py

--- my_question/array-next-greater.py
+++ my_question/array-next-greater.py
@@ -1,22 +1,24 @@
 # https://www.geeksforgeeks.org/next-greater-element/
 
 a = [8,2,7,5,1,3]
-#a = [4,5,2,25]
+a = [4,5,2,25]
 stack = []
 
 stack.append(a[0])
 
-for next_ in range(1,len(a)-1):
-    if len(a)==0:
-        pass
-    else:
+for next_ in range(1,len(a)):
+    if len(stack)!=0:
         element = stack.pop()
-        if element <a[next_]:
-            stack.append(a[next_])
+        while element < a[next_]:
             print('for {} next greater is {}'.format(element,a[next_]))
-        else:
+            if len(stack)==0:
+                break
+            element = stack.pop()
+            
+        if element > a[next_]:
             stack.append(element)
-            stack.append(a[next_])
-if len(a)!=0:
+    stack.append(a[next_])
+    
+if len(stack)!=0:
     for data in stack:
-        print("For {} next greater is {}".format(data,-1))        
+        print("For {} next greater is {}".format(data,-1))
