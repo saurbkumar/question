d2aee6b69944f6218bd400ec46311b46d281d585
Saurabh Kumar <saurabhkumar.spsu@gmail.com>
2019-01-30 07:47:48 -0800

    Create binary-search-tree-inorder-successor-in.py

---
+++ my_question/binary-search-tree-inorder-successor-in.py
@@ -0,0 +1,7 @@
+# https://www.geeksforgeeks.org/inorder-successor-in-binary-search-tree/
+
+'''
+Idea: If target node has right sub-tree, then the smallest element in that is the inorder successor, if node does not has left sub tree
+then the immediate parent is the inorder successor
+'''
+
